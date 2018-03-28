fb95c825d9169f284dedee79c236461b4b340c5b
Chase <ChaseEWhitney@gmail.com>
2018-03-28 12:33:58 -0500

    default user goals created at registration

---
+++ server/models/userGoals.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+// Mongoose Schema
+var UserGoalsSchema = new Schema({
+    username: {type: String, required: true},
+    calories: {type: Array},
+    cholesterol: {type: Array},
+    fiber: {type: Array},
+    protein: {type: Array},
+    sodium: {type: Array},
+    sugar: {type: Array},
+    carbohydrates: {type: Array},
+    fat: {type: Array},
+});
+
+
+module.exports = mongoose.model('UserGoals', UserGoalsSchema);
--- server/routes/register.router.js
+++ server/routes/register.router.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Users = require('../models/user.js');
+var UserGoals = require('../models/userGoals.js');
 var path = require('path');
 
 
@@ -23,6 +24,18 @@ router.post('/', function(req, res, next) {
       password : req.body.password
     };
 
+    var defaultUserGoals = {
+        username: req.body.username,
+        calories: [2000,2000],
+        cholesterol: [300,300],
+        fiber: [25,25],
+        protein: [50,50],
+        sodium: [2400,2400],
+        sugar: [0,0],
+        carbohydrates: [300,300],
+        fat: [65,65],
+    };
+
 
     Users.create(userToSave, function(err, post) {
       console.log('post /register -- User.create');
@@ -33,7 +46,19 @@ router.post('/', function(req, res, next) {
          } else {
            console.log('post /register -- User.create -- success');
           // route a new express request for GET '/'
-          res.redirect('/');
+
+          UserGoals.create(defaultUserGoals, function(err, post) {
+            console.log('post /register -- UserGoals.create');
+               if(err) {
+                 console.log('post /register -- UserGoals.create -- failure');
+                 // next() here would continue on and route to routes/index.js
+                 next(err);
+               } else {
+                 console.log('post /register -- UserGoals.create -- success');
+                // route a new express request for GET '/'
+                res.redirect('/');
+               }
+          });
          }
     });
 });
