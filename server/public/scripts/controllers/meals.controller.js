933f85ca47460cdb3b20e4e8280e47060a99b8a7
Chase <ChaseEWhitney@gmail.com>
2019-12-02 14:15:01 -0600

    UI update

--- server/public/scripts/controllers/meals.controller.js
+++ server/public/scripts/controllers/meals.controller.js
@@ -1,35 +1,53 @@
-myApp.controller('MealsController', function(UserService, MealsService, $http, $mdDialog, $mdPanel, $scope, $modal, $log) {
-  console.log('MealsController created');
+myApp.controller("MealsController", function(
+  UserService,
+  MealsService,
+  $http,
+  $mdDialog,
+  $mdPanel,
+  $scope,
+  $modal,
+  $log
+) {
+  console.log("MealsController created");
   var vm = this;
   vm.userService = UserService;
   vm.userObject = UserService.userObject;
   vm.ms = MealsService;
 
-/// TO DO ///
-/// CONFIRMATION DIALOGS
-/// CLICK/HOVER TO ENLARGE API IMAGES
-/// HISTORICAL ENTRIES EDITABLE
-/// REWORK CALCDAILYTOTALS FUNCTION
-/// STYLING/LAYOUT
-/// VALIDATION FOR ENTRIES
+  var date = new Date("2010-10-11T00:00:00+05:30");
+  vm.todayDate =
+    (date.getMonth() > 8 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)) +
+    "/" +
+    (date.getDate() > 9 ? date.getDate() : "0" + date.getDate()) +
+    "/" +
+    date.getFullYear();
 
+  /// TO DO ///
+  /// CONFIRMATION DIALOGS
+  /// CLICK/HOVER TO ENLARGE API IMAGES
+  /// HISTORICAL ENTRIES EDITABLE
+  /// REWORK CALCDAILYTOTALS FUNCTION
+  /// STYLING/LAYOUT
+  /// VALIDATION FOR ENTRIES
 
   getGoals();
   getTodayProgress();
   getFavorites();
 
   // Determines caloric content for each meal entry
-  function calcCaloricComposition(today){
+  function calcCaloricComposition(today) {
     // console.log('calculation caloric compositions');
     for (var i = 0; i < today.length; i++) {
       today[i].fatPercent = ((9 * today[i].fat) / today[i].calories) * 100;
-      today[i].proteinPercent = ((4 * today[i].protein) / today[i].calories) * 100;
-      today[i].carbPercent = 100 - today[i].fatPercent - today[i].proteinPercent;
+      today[i].proteinPercent =
+        ((4 * today[i].protein) / today[i].calories) * 100;
+      today[i].carbPercent =
+        100 - today[i].fatPercent - today[i].proteinPercent;
     }
   }
 
   // Calculates nutrient totals for the day //// REWORK THIS FUNCTION
-  function calcDailyTotal(today){
+  function calcDailyTotal(today) {
     // console.log('calculating totals');
     vm.todayTotal = angular.copy(today[0]);
     for (var i = 1; i < today.length; i++) {
@@ -42,21 +60,21 @@ myApp.controller('MealsController', function(UserService, MealsService, $http, $
 
   // Creates modal for editing a Favorites entry
   function editFavoriteModal(ev) {
-    console.log('in editFavoriteModal editing:', vm.ms.favObject);
+    console.log("in editFavoriteModal editing:", vm.ms.favObject);
     $mdDialog.show({
       controller: DialogController,
-      templateUrl: '/views/partials/editFavorite.html',
+      templateUrl: "/views/partials/editFavorite.html",
       parent: angular.element(document.body),
       targetEvent: ev,
-      clickOutsideToClose:true,
+      clickOutsideToClose: true,
       fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
     });
   }
 
   // Gets user's saved favorites
-  function getFavorites(){
-    $http.get('/meals/getFavorites').then(function(response){
-      console.log('getFavorites response data is:', response.data);
+  function getFavorites() {
+    $http.get("/meals/getFavorites").then(function(response) {
+      console.log("getFavorites response data is:", response.data);
       vm.favorites = response.data;
       for (var i = 0; i < vm.favorites.length; i++) {
         vm.favorites[i].servings = 1;
@@ -65,17 +83,17 @@ myApp.controller('MealsController', function(UserService, MealsService, $http, $
   }
 
   // Gets user's nutritional goals
-  function getGoals(){
-    $http.get('/goals').then(function(response){
+  function getGoals() {
+    $http.get("/goals").then(function(response) {
       vm.goals = response.data[0];
     });
   }
 
   // Gets meal history for today
-  function getTodayProgress(){
-    $http.get('/meals/getTodayProgress').then(function(response){
+  function getTodayProgress() {
+    $http.get("/meals/getTodayProgress").then(function(response) {
       vm.today = response.data;
-      console.log('vm.today is:', vm.today);
+      console.log("vm.today is:", vm.today);
       calcDailyTotal(vm.today);
       calcCaloricComposition(vm.today);
     });
@@ -85,31 +103,31 @@ myApp.controller('MealsController', function(UserService, MealsService, $http, $
   vm.addEntryModal = function(ev) {
     $mdDialog.show({
       controller: DialogController,
-      templateUrl: '/views/partials/addMealTemplate.html',
+      templateUrl: "/views/partials/addMealTemplate.html",
       parent: angular.element(document.body),
       targetEvent: ev,
-      clickOutsideToClose:true,
+      clickOutsideToClose: true,
       fullscreen: false // Only for -xs, -sm breakpoints.
     });
   };
 
   // Brings up modal with Favorites entry information and options
   vm.clickFavoriteModal = function(ev, favObject) {
-    console.log('favorite clicked:', favObject);
+    console.log("favorite clicked:", favObject);
     vm.ms.favObject = favObject;
 
     $mdDialog.show({
       controller: DialogController,
-      templateUrl: '/views/partials/addFromFavorites.html',
+      templateUrl: "/views/partials/addFromFavorites.html",
       parent: angular.element(document.body),
       targetEvent: ev,
-      clickOutsideToClose:true,
+      clickOutsideToClose: true,
       fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
     });
   };
 
   // Send a meal entry to be added to the database
-  vm.createMealEntry = function(name, servingSize, servings, meal){
+  vm.createMealEntry = function(name, servingSize, servings, meal) {
     mealToEnter = meal;
     for (var key in mealToEnter) {
       mealToEnter[key] *= servings;
@@ -118,67 +136,74 @@ myApp.controller('MealsController', function(UserService, MealsService, $http, $
     mealToEnter.servingSize = servingSize;
     mealToEnter.servings = servings;
 
-    console.log('in createMealEntry sending mealToEnter:', mealToEnter);
-    $http.post('/meals/createEntry', mealToEnter).then(function(response){
-      console.log('got response from POST /meals/createEntry');
+    console.log("in createMealEntry sending mealToEnter:", mealToEnter);
+    $http.post("/meals/createEntry", mealToEnter).then(function(response) {
+      console.log("got response from POST /meals/createEntry");
       getTodayProgress();
     });
   };
 
-
   // Delete a meal entry from the database
-  vm.deleteEntry = function(entry){
-    $http.delete('/meals/deleteEntry/' + entry._id).then(function(response){
-      console.log('Entry deleted.');
+  vm.deleteEntry = function(entry) {
+    $http.delete("/meals/deleteEntry/" + entry._id).then(function(response) {
+      console.log("Entry deleted.");
       getTodayProgress();
       $mdDialog.hide();
     });
   };
 
   // Update a meal entry in the database
-  vm.editEntry = function(ev, entry){
+  vm.editEntry = function(ev, entry) {
     vm.ms.entryToEdit = entry;
     $mdDialog.show({
       controller: DialogController,
-      templateUrl: '/views/partials/editEntry.html',
+      templateUrl: "/views/partials/editEntry.html",
       parent: angular.element(document.body),
       targetEvent: ev,
-      clickOutsideToClose:true,
+      clickOutsideToClose: true,
       fullscreen: false // Only for -xs, -sm breakpoints.
     });
   };
 
-
   // Run when user enters text into the API search dropdown
-  vm.instantSearch = function(toQuery){
+  vm.instantSearch = function(toQuery) {
     console.log("in instantSearch with:", toQuery);
-    vm.instantData ={};
+    vm.instantData = {};
     if (toQuery.length > 2) {
-      var config = {params: {searchQuery: toQuery,}};
-      $http.get('api/instant', config).then(function(response){
-        console.log('Response from api/instant GET');
+      var config = { params: { searchQuery: toQuery } };
+      $http.get("api/instant", config).then(function(response) {
+        console.log("Response from api/instant GET");
         vm.instantData = response.data;
-        console.log('response:', response);
-        console.log('vm.instantData:', vm.instantData);
+        console.log("response:", response);
+        console.log("vm.instantData:", vm.instantData);
       });
     }
   };
 
   // Brings up the options menu for meal entries
   vm.entryOptionsMenu = function($mdMenu, ev, item) {
-    console.log('in entryOptionsMenu(), item is:', item);
+    console.log("in entryOptionsMenu(), item is:", item);
     $mdMenu.open(ev);
     vm.menuItem = item;
-    console.log('vm.menuItem is:', vm.menuItem);
+    console.log("vm.menuItem is:", vm.menuItem);
   };
 
-  function formatEntry(obj, math){
-    var props = ["calories", "carbohydrates", "fat", "fiber", "protein", "sodium", "sugar"];
+  function formatEntry(obj, math) {
+    var props = [
+      "calories",
+      "carbohydrates",
+      "fat",
+      "fiber",
+      "protein",
+      "sodium",
+      "sugar"
+    ];
     try {
-      if(math != "per serving" && math != "total") throw "Illegal math argument sent to formatEntry";
-      if(obj._id) delete obj._id;
-      if(obj.$$hashKey) delete obj.$$hashKey;
-      if(math == "per serving"){
+      if (math != "per serving" && math != "total")
+        throw "Illegal math argument sent to formatEntry";
+      if (obj._id) delete obj._id;
+      if (obj.$$hashKey) delete obj.$$hashKey;
+      if (math == "per serving") {
         for (var i = 0; i < props.length; i++) {
           obj[props[i]] /= obj.servings;
         }
@@ -187,45 +212,48 @@ myApp.controller('MealsController', function(UserService, MealsService, $http, $
           obj[props[i]] *= obj.servings;
         }
       }
-    } catch(err) {
+    } catch (err) {
       console.log(err);
     }
   }
 
   // Save an entry to favorites
-  vm.saveToFavorites = function(item){
-    console.log('in saveToFavorites with:', item);
+  vm.saveToFavorites = function(item) {
+    console.log("in saveToFavorites with:", item);
     formatEntry(item, "per serving");
-    console.log('sending to POST addFavorite:', item);
-    $http.post('/meals/addFavorite', item).then(function(response){
-      console.log('got response from POST /meals/addFavorite');
+    console.log("sending to POST addFavorite:", item);
+    $http.post("/meals/addFavorite", item).then(function(response) {
+      console.log("got response from POST /meals/addFavorite");
       getFavorites();
       ///// ADD CONFIRMATION DIALOG /////
     });
   };
 
   // Run when a food item is selected from API search dropdown
-  vm.selectAPIItem = function(ev, itemID, type){
+  vm.selectAPIItem = function(ev, itemID, type) {
     // console.log(`in selectAPI with: ${type} ${itemID}`);
-    var config = {params:{toQuery:itemID}};
-    $http.get('/api/' + type, config).then(function(response){
-      console.log('/api/' + type + ' GET response usable data:', response.data.foods[0]);
+    var config = { params: { toQuery: itemID } };
+    $http.get("/api/" + type, config).then(function(response) {
+      console.log(
+        "/api/" + type + " GET response usable data:",
+        response.data.foods[0]
+      );
       var data = response.data.foods[0];
       vm.ms.apiFoodObject = createFoodObject(data);
-      vm.apiSearchText = '';
+      vm.apiSearchText = "";
       $mdDialog.show({
         controller: DialogController,
-        templateUrl: '/views/partials/addFromAPI.html',
+        templateUrl: "/views/partials/addFromAPI.html",
         parent: angular.element(document.body),
         targetEvent: ev,
-        clickOutsideToClose:true,
+        clickOutsideToClose: true,
         fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
       });
     });
   };
 
-  function createFoodObject(data){
-    console.log('in createFoodObject with:', data);
+  function createFoodObject(data) {
+    console.log("in createFoodObject with:", data);
     var foodObj = {};
     foodObj.name = data.food_name;
     foodObj.servingSize = data.serving_qty + data.serving_unit;
@@ -241,11 +269,9 @@ myApp.controller('MealsController', function(UserService, MealsService, $http, $
   }
 
   //// TEST FUNCTION ////
-  vm.test = function(){
-    console.log('in test');
+  vm.test = function() {
+    console.log("in test");
   };
-
-
 
   function DialogController(MealsService, $scope, $mdDialog) {
     $scope.favObject = MealsService.favObject;
@@ -255,7 +281,6 @@ myApp.controller('MealsController', function(UserService, MealsService, $http, $
     $scope.edit = function(ev) {
       $mdDialog.hide();
       editFavoriteModal(ev);
-
     };
 
     $scope.hide = function() {
@@ -268,21 +293,21 @@ myApp.controller('MealsController', function(UserService, MealsService, $http, $
 
     $scope.delete = function(fav) {
       console.log("in delete with:", fav);
-      $http.delete('/meals/deleteFavorite/' + fav._id).then(function(response){
-        console.log('Favorite deleted.');
+      $http.delete("/meals/deleteFavorite/" + fav._id).then(function(response) {
+        console.log("Favorite deleted.");
         getFavorites();
         $mdDialog.hide();
       });
     };
 
     $scope.createEntry = function(name, servingSize, servings, meal) {
-      console.log('name, size, servings:', name, servingSize, servings, meal);
+      console.log("name, size, servings:", name, servingSize, servings, meal);
       vm.createMealEntry(name, servingSize, servings, meal);
       $mdDialog.hide();
     };
 
     $scope.createEntryFavorite = function(name, servingSize, servings, meal) {
-      console.log('name, size, servings:', name, servingSize, servings, meal);
+      console.log("name, size, servings:", name, servingSize, servings, meal);
       vm.createMealEntry(name, servingSize, servings, meal);
       var favMeal = meal;
       favMeal.name = name;
@@ -291,33 +316,32 @@ myApp.controller('MealsController', function(UserService, MealsService, $http, $
       $mdDialog.hide();
     };
 
-    $scope.addEntry = function(foodObj){
-      console.log('ADDING ENTRY FOODOBJ', foodObj);
+    $scope.addEntry = function(foodObj) {
+      console.log("ADDING ENTRY FOODOBJ", foodObj);
       formatEntry(foodObj, "total");
-      $http.post('/meals/createEntry', foodObj).then(function(response){
-        console.log('got response from POST /meals/createEntry');
+      $http.post("/meals/createEntry", foodObj).then(function(response) {
+        console.log("got response from POST /meals/createEntry");
         $mdDialog.hide();
         getTodayProgress();
       });
     };
 
-    $scope.updateFavorite = function(fav){
-      console.log('updating favorite:', fav);
-      $http.put('/meals/updateFavorite', fav).then(function(response){
-        console.log('got response from PUT /meals/updateFavorite');
+    $scope.updateFavorite = function(fav) {
+      console.log("updating favorite:", fav);
+      $http.put("/meals/updateFavorite", fav).then(function(response) {
+        console.log("got response from PUT /meals/updateFavorite");
         $mdDialog.hide();
         getFavorites();
       });
     };
 
-    $scope.updateEntry = function(entry){
-      console.log('updating entry:', entry);
-      $http.put('/meals/updateEntry', entry).then(function(response){
-        console.log('got response from PUT /meals/updateEntry');
+    $scope.updateEntry = function(entry) {
+      console.log("updating entry:", entry);
+      $http.put("/meals/updateEntry", entry).then(function(response) {
+        console.log("got response from PUT /meals/updateEntry");
         $mdDialog.hide();
         getTodayProgress();
       });
     };
   }
-
 });
--- server/server.js
+++ server/server.js
@@ -1,30 +1,29 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
-var passport = require('./strategies/mongo.localstrategy');
-var sessionConfig = require('./modules/session.config');
+var passport = require("./strategies/mongo.localstrategy");
+var sessionConfig = require("./modules/session.config");
 
 //DB Module
-var db = require('./modules/db.config.js');
+var db = require("./modules/db.config.js");
 
 // Route includes
-var indexRouter = require('./routes/index.router');
-var userRouter = require('./routes/user.router');
-var registerRouter = require('./routes/register.router');
-var goalsRouter = require('./routes/goals.router');
-var mealsRouter = require('./routes/meals.router');
-var apiRouter = require('./routes/api.router');
+var indexRouter = require("./routes/index.router");
+var userRouter = require("./routes/user.router");
+var registerRouter = require("./routes/register.router");
+var goalsRouter = require("./routes/goals.router");
+var mealsRouter = require("./routes/meals.router");
+var apiRouter = require("./routes/api.router");
 
-var port = process.env.PORT || 5000;
-
+var port = process.env.PORT || 5001;
 
 // Body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve back static files
-app.use(express.static('./server/public'));
+app.use(express.static("./server/public"));
 
 // Passport Session Configuration
 app.use(sessionConfig);
@@ -34,16 +33,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.use('/register', registerRouter);
-app.use('/user', userRouter);
-app.use('/goals', goalsRouter);
-app.use('/meals', mealsRouter);
-app.use('/api', apiRouter);
+app.use("/register", registerRouter);
+app.use("/user", userRouter);
+app.use("/goals", goalsRouter);
+app.use("/meals", mealsRouter);
+app.use("/api", apiRouter);
 
 // Catch all bucket, must be last!
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // Listen //
-app.listen(port, function(){
-   console.log('Listening on port:', port);
+app.listen(port, function() {
+  console.log("Listening on port:", port);
 });
